6370b21257ea21fec6ef0c1343fa227223db5706
mr-abims <adebayoabimbola9@gmail.com>
2022-03-27 03:56:42 +0100

    Details card

--- src/App.js
+++ src/App.js
@@ -6,10 +6,10 @@ import {useState, useEffect} from 'react'
 import Footer from './components/Footer/Footer';
 import { ethers, utils, Contract } from 'ethers';
 import BRTTokenAbi from './utils/web3/abi.json'
+import { formatDate } from "./utils/helpers";
 const BRTTokenAddress = "0x169E82570feAc981780F3C48Ee9f05CED1328e1b";
 
 function App() {
-
   // a flag for keeping track of whether or not a user is connected
   const [connected, setConnected] = useState(false);
 
@@ -17,15 +17,14 @@ function App() {
   const [userInfo, setUserInfo] = useState({
     matic_balance: 0,
     token_balance: 0,
-    address: null
+    address: null,
   });
-  
-  
-  // the amount of token the user have staked
-  const [stakeAmount, setStakeAmount] = useState(null)
 
+  // the amount of token the user have staked
+  const [stakeAmount, setStakeAmount] = useState(null);
+  const [addressInput, setAddressInput] = useState("");
   // the amount of reward the user has accumulate on his stake
-  const [rewardAmount, setRewardAmount] = useState(null)
+  const [rewardAmount, setRewardAmount] = useState(null);
 
   // the value of token the user wants to stake
   const [stakeInput, setStakeInput] = useState("");
@@ -36,6 +35,7 @@ function App() {
   // all stake history data displayed on the history table
   const [stateHistory, setStakeHistory] = useState([]);
 
+  const [stakeDetails, setStakeDetails] = useState({});
   // helper function for getting the matic and token balance, given an address
   const getAccountDetails = async (address) => {
     try {
@@ -186,7 +186,9 @@ function App() {
       case "unstake":
         setWithdrawInput(target.value);
         break;
-
+      case "address":
+        setAddressInput(target.value);
+        break;
       default:
         break;
     }
@@ -224,8 +226,27 @@ function App() {
       BRTTokenAbi,
       signer
     );
-    const MyState = await BRTContractInstance.myStake();
-    setStakeAmount(utils.formatUnits(MyState.stakeAmount, 18));
+    const myStake = await BRTContractInstance.myStake();
+    const stake = utils.formatUnits(myStake.stakeAmount, 18);
+    setStakeAmount(stake);
+
+    // getting the last stake in seconds
+    const lastestStake = formatDate(myStake.time.toString());
+    const newStakeTime = new Date(lastestStake);
+    const stakeSeconds = Math.floor(newStakeTime.getTime() / 1000);
+
+    // getting the current day in seconds
+    const currentDay = new Date();
+    const currentDaySeconds = Math.floor(currentDay.getTime() / 1000);
+
+    // getting the difference between the lastest stake and the current day
+    const timeDifference = currentDaySeconds - stakeSeconds;
+
+    // showing reward after 3 days otherwise showing 0
+    if (timeDifference >= 259200) {
+      const reward = 0.0000000386 * timeDifference * stake;
+      setRewardAmount(reward.toFixed(3));
+    } else setRewardAmount("00.00");
   };
 
   const onClickWithdraw = async (e) => {
@@ -246,10 +267,33 @@ function App() {
     const stakeTxHash = await provider.getTransaction(stakeTx.hash);
     const response = await stakeTx.wait();
     await getStake();
+  };
+  const onClickAddress = async (e) => {
+    if (addressInput === "") {
+      return alert("Input field cannot be empty!");
+    }
+    e.preventDefault();
+    const customProvider = new ethers.providers.JsonRpcProvider(
+      process.env.REACT_APP_RPC_URL
+    );
+    const BRTContractInstance = new Contract(
+      BRTTokenAddress,
+      BRTTokenAbi,
+      customProvider
+    );
+    const userDetails = await BRTContractInstance.getStakeByAddress(
+      addressInput
+    );
+    console.log(userDetails);
+    // setStakeDetails( (parseInt(userDetails.stakeAmount._hex)/ 1e18).toFixed(2))
+    setStakeDetails({
+      address: userDetails.staker,
+      amount: utils.formatUnits(userDetails.stakeAmount.toString(), 18),
+      time: formatDate(userDetails.time._hex),
+    });
 
-    const address = response.events[1].args[0];
-    const amountStaked = response.events[1].args[1].toString();
-    const time = response.events[1].args[2].toString();
+    // console.log(stakeDetails)
+    setAddressInput("");
   };
 
   return (
@@ -266,6 +310,8 @@ function App() {
           onChangeInput={onChangeInput}
           onClickStake={onClickStake}
           onClickWithdraw={onClickWithdraw}
+          onClickAddress={onClickAddress}
+          stakeDetails={stakeDetails}
           stakeAmount={stakeAmount}
           rewardAmount={rewardAmount}
           connected={connected}
